c608e6f67bac414f57c39d093e1381369f08b46e
qianshusu <146901434+qianshusu@users.noreply.github.com>
2026-05-27 16:37:02 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,292 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    "红牛资源": {
+        api: "http://hongniuzy2.com/api.php/provide/vod/from/hnm3u8",
+        名称: "红牛资源"
+    },
+    "量子资源": {
+        api: "http://cj.lziapi.com/api.php/provide/vod/from/lzm3u8",
+        名称: "量子资源"
+    },
+    "开放电影": {
+        api: "http://vod-demo.onrender.com/pubdovod.php",
+        名称: "开放电影"
+    },
+    "优质资源库": {
+        api: "http://api.1080zyku.com/inc/api.php/provide/vod",
+        名称: "优质资源库"
+    },
+    "39影视": {
+        api: "http://39kan.com/api.php/provide/vod",
+        名称: "39影视"
+    },
+    "天堂资源": {
+        api: "http://vipmv.cc/api.php/provide/vod",
+        名称: "天堂资源"
+    },
+    "乐活影视": {
+        api: "http://lehootv.com/api.php/provide/vod",
+        名称: "乐活影视"
+    },
+    "唐人街": {
+        api: "shturl.cc/aCnqk1gU2APsEK7DoUbpKCRi3ZdMu0",
+        名称: "唐人街"
+    },
+    "酷点资源": {
+        api: "http://api.kuapi.cc/api.php/provide/vod",
+        名称: "酷点资源"
+    },
+    "卧龙资源": {
+        api: "http://collect.wolongzyw.com/api.php/provide/vod",
+        名称: "卧龙资源"
+    },
+    "森林资源": {
+        api: "shturl.cc/yU81Bn9NHJQiAGNUyES8THNJEUs5",
+        名称: "森林资源"
+    },
+    "影库资源网": {
+        api: "http://api.ykapi.net/api.php/provide/vod",
+        名称: "影库资源网"
+    },
+    "快车资源": {
+        api: "http://caiji.kczyapi.com/api.php/provide/vod/from/kcm3u8",
+        名称: "快车资源"
+    },
+    "探探资源": {
+        api: "shturl.cc/TByH12perPhXPn8vRTCC38vCmkoM",
+        名称: "探探资源"
+    },
+    "闪电资源": {
+        api: "http://sdzyapi.com/api.php/provide/vod/from/sdm3u8",
+        名称: "闪电资源"
+    },
+    "金鹰资源": {
+        api: "http://jyzyapi.com/provide/vod/from/jinyingm3u8",
+        名称: "金鹰资源"
+    },
+    "光速资源": {
+        api: "http://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8",
+        名称: "光速资源"
+    },
+    "奥斯卡资源网": {
+        api: "http://aosikazy.com/api.php/provide/vod",
+        名称: "奥斯卡资源网"
+    },
+    "老鸭资源采集": {
+        api: "http://api.apilyzy.com/api.php/provide/vod",
+        名称: "老鸭资源采集"
+    },
+    "U酷资源": {
+        api: "http://api.ukuapi.com/api.php/provide/vod",
+        名称: "U酷资源"
+    },
+    "北斗星资源": {
+        api: "http://m3u8.bdxzyapi.com/api.php/provide/vod",
+        名称: "北斗星资源"
+    },
+    "樱花资源网": {
+        api: "http://m3u8.apiyhzy.com/api.php/provide/vod",
+        名称: "樱花资源网"
+    },
+    "飞速资源": {
+        api: "http://feisuzy.com/api.php/provide/vod",
+        名称: "飞速资源"
+    },
+    "快播资源": {
+        api: "http://www.kuaibozy.com/api.php/provide/vod",
+        名称: "快播资源"
+    },
+    "百度资源": {
+        api: "http://api.apibdzy.com/api.php/provide/vod",
+        名称: "百度资源"
+    },
+    "艾旦影视": {
+        api: "http://lovedan.net/api.php/provide/vod",
+        名称: "艾旦影视"
+    },
+    "飘花电影": {
+        api: "http://www.zzrhgg.com/api.php/provide/vod",
+        名称: "飘花电影"
+    },
+    "网民电影": {
+        api: "http://prinevillesda.org/api.php/provide/vod",
+        名称: "网民电影"
+    },
+    "丝袜资源": {
+        api: "http://siwazyw.cc/api.php/provide/vod",
+        名称: "丝袜资源"
+    },
+    "天空资源": {
+        api: "http://m3u8.tiankongapi.com/api.php/provide/vod/from/tkm3u8",
+        名称: "天空资源"
+    },
+    "海外看资源": {
+        api: "http://haiwaikan.com/api.php/provide/vod",
+        名称: "海外看资源"
+    },
+    "海外看点播": {
+        api: "https://haiwaikan.com/api.php/provide/vod/",
+        名称: "海外看点播"
+    },
+    "360点播": {
+        api: "https://360zy.com/api.php/provide/vod/",
+        名称: "360点播"
+    },
+    "黑木耳点播": {
+        api: "https://www.heimuer.tv/api.php/provide/vod",
+        名称: "黑木耳点播"
+    },
+    "刺桐点播": {
+        api: "http://ys9.cttv.vip/api.php/provide/vod/",
+        名称: "刺桐点播"
+    },
+    "官网点播": {
+        api: "http://gwcms.cttv.vip/api.php/provide/vod/",
+        名称: "官网点播"
+    },
+    "业余点播": {
+        api: "https://yyff.540734621.xyz/api.php/provide/vod/",
+        名称: "业余点播"
+    },
+    "华为吧点播": {
+        api: "https://hw8.live/api.php/provide/vod/",
+        名称: "华为吧点播"
+    },
+    "小黄人点播": {
+        api: "https://iqyi.xiaohuangrentv.com/api.php/provide/vod/",
+        名称: "小黄人点播"
+    },
+    "牛牛点播": {
+        api: "https://api.niuniuzy.me/api.php/provide/vod/",
+        名称: "牛牛点播"
+    },
+    "丫丫点播": {
+        api: "https://cj.yayazy.net/api.php/provide/vod/",
+        名称: "丫丫点播"
+    },
+    "U酷点播": {
+        api: "https://api.ukuapi.com/api.php/provide/vod/",
+        名称: "U酷点播"
+    },
+    "豪华点播": {
+        api: "https://hhzyapi.com/api.php/provide/vod",
+        名称: "豪华点播"
+    },
+    "极速点播": {
+        api: "https://jszyapi.com/api.php/provide/vod",
+        名称: "极速点播"
+    },
+    "四九点播": {
+        api: "https://49zyw.com/api.php/provide/vod/",
+        名称: "四九点播"
+    },
+    "索尼点播": {
+        api: "https://suoniapi.com/api.php/provide/vod/",
+        名称: "索尼点播"
+    },
+    "ikun点播": {
+        api: "https://ikunzyapi.com/api.php/provide/vod/",
+        名称: "ikun点播"
+    },
+    "非凡点播": {
+        api: "http://cj.ffzyapi.com/api.php/provide/vod/",
+        名称: "非凡点播"
+    },
+    "量子点播": {
+        api: "https://cj.lziapi.com/api.php/provide/vod/",
+        名称: "量子点播"
+    },
+    "暴风点播": {
+        api: "https://bfzyapi.com/api.php/provide/vod/",
+        名称: "暴风点播"
+    },
+    "红牛点播": {
+        api: "https://www.hongniuzy2.com/api.php/provide/vod/",
+        名称: "红牛点播"
+    },
+    "飞速点播": {
+        api: "https://www.feisuzyapi.com/api.php/provide/vod/",
+        名称: "飞速点播"
+    },
+    "快看点播": {
+        api: "https://www.kuaikan-api.com/api.php/provide/vod/",
+        名称: "快看点播"
+    },
+    "熊掌点播": {
+        api: "https://xzcjz.com/api.php/provide/vod/",
+        名称: "熊掌点播"
+    },
+    "快车点播": {
+        api: "https://caiji.kczyapi.com/api.php/provide/vod/from/kcm3u8/",
+        名称: "快车点播"
+    },
+    "闪电点播": {
+        api: "http://sdzyapi.com/api.php/provide/vod/",
+        名称: "闪电点播"
+    },
+    "樱花点播": {
+        api: "https://m3u8.apiyhzy.com/api.php/provide/vod/",
+        名称: "樱花点播"
+    },
+    "卧龙点播": {
+        api: "https://collect.wolongzyw.com/api.php/provide/vod/",
+        名称: "卧龙点播"
+    },
+    "飘花点播": {
+        api: "http://www.ahjiuman.com/api.php/provide/vod/at/json",
+        名称: "飘花点播"
+    },
+    "天翼点播": {
+        api: "https://www.911ysw.top/tianyi.php/provide/vod/",
+        名称: "天翼点播"
+    },
+    "虎牙点播": {
+        api: "https://www.huyaapi.com/api.php/provide/vod/",
+        名称: "虎牙点播"
+    },
+    "百度点播": {
+        api: "https://api.apibdzy.com/api.php/provide/vod/",
+        名称: "百度点播"
+    },
+    "飘零点播": {
+        api: "https://p2100.net/api.php/provide/vod/",
+        名称: "飘零点播"
+    },
+    "无尽点播": {
+        api: "https://api.wujinapi.com/api.php/provide/vod/",
+        名称: "无尽点播"
+    },
+    "速博点播": {
+        api: "https://subocaiji.com/api.php/provide/vod/",
+        名称: "速博点播"
+    },
+    "魔都点播": {
+        api: "https://caiji.moduapi.cc/api.php/provide/vod/",
+        名称: "魔都点播"
+    },
+    "最大点播": {
+        api: "http://zuidazy.me/api.php/provide/vod/",
+        名称: "最大点播"
+    },
+    "奇虎点播": {
+        api: "https://caiji.qhzyapi.com/api.php/provide/vod/",
+        名称: "奇虎点播"
+    },
+    "火狐点播": {
+        api: "https://hhzyapi.com/api.php/provide/vod/",
+        名称: "火狐点播"
+    },
+    "新浪点播": {
+        api: "https://api.xinlangapi.com/xinlangapi.php/provide/vod/",
+        名称: "新浪点播"
+    },
+    "快云点播": {
+        api: "https://www.kuaiyunzy.com/api.php/provide/vod/",
+        名称: "快云点播"
+    },
+    // 保留原来的七七资源
+    "七七": {
+        api: "https://www.qiqidys.com/api.php/provide/vod",
+        名称: "七七资源"
     }
 };
 
